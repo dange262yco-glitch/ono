6593c164dc9859f8c2f702dcc737c175f8195dd2
dange262yco-glitch <dange262yco@gmail.com>
2025-11-22 02:44:23 +0300

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { createClient } from "@valkey/client";
+import { createClient } from "valkey";
 
 const app = express();
 app.use(express.json());
